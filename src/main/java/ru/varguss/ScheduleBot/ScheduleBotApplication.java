6f909c6304a0c599be59eb953379b548174d4b32
Varguss <varablej@gmail.com>
2019-01-25 16:48:56 +0300

    Spring initializer

---
+++ src/main/java/ru/varguss/ScheduleBot/ScheduleBotApplication.java
@@ -0,0 +1,14 @@
+package ru.varguss.ScheduleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleBotApplication.class, args);
+	}
+
+}
+
